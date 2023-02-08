4c57087854b5ce6357ce8449a657b08ab2876a67
Muhammad Imam Muwatok <45243273+imam-muwatok@users.noreply.github.com>
2023-02-08 08:04:44 +0700

    Create apache2.sh

---
+++ apache2.sh
@@ -0,0 +1,14 @@
+#path home
+/data/data/com.termux/files/home
+
+#path file android
+/data/data/com.termux/files/home/storage/shared
+
+#path bin
+/data/data/com.termux/files/usr/bin
+
+#path apache2 conf
+/data/data/com.termux/files/usr/etc/apache2/
+
+#run apache2
+apachectl
