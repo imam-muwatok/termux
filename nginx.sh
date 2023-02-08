34ef85df64ecc9af7e27bc424588381d937234be
Muhammad Imam Muwatok <45243273+imam-muwatok@users.noreply.github.com>
2023-02-08 07:12:48 +0700

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,4 +1,11 @@
-/data/data/com.termux/files/usr/etc/nginx/
+#path nginx.conf
+cd /data/data/com.termux/files/usr/etc/nginx/
 
 #cp origin config
 cp nginx.conf{,.orig}
+
+#for run nginx
+nginx
+
+#for stop nginx
+nginx -s stop
